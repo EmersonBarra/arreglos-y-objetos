62d4251e3bdbac3f8a7dc2c406df382cf6adf217
unknown <emerson.barra.1991@gmail.com>
2024-10-06 22:44:03 -0300

    first commit

---
+++ assets/js/propiedades.js
@@ -0,0 +1,70 @@
+export const propiedades_alquiler = [
+    {
+        nombre: "Apartamento en alquiler 1",
+        src: "https://images.pexels.com/photos/1029599/pexels-photo-1029599.jpeg?auto=compress&cs=tinysrgb&w=600",
+        descripcion: "Este apartamento de lujo está ubicado en una exclusiva zona residencial",
+        ubicacion: 'Los Lagos, Chile',
+        habitaciones: 4,
+        banos: 3,
+        costo: 5000,
+        smoke: false,
+        pets: false,
+    },
+    {
+        nombre: "Apartamento en alquiler 2",
+        src: 'https://images.pexels.com/photos/1396122/pexels-photo-1396122.jpeg?auto=compress&cs=tinysrgb&w=600',
+        descripcion: "Este apartamento de lujo está ubicado en una exclusiva zona residencial",
+        ubicacion: 'La Araucanía, Chile',
+        habitaciones: 5,
+        banos: 2,
+        costo: 2500,
+        smoke: true,
+        pets: true
+    },
+    {
+        nombre: "Apartamento en alquiler 3",
+        src: 'https://images.pexels.com/photos/259588/pexels-photo-259588.jpeg?auto=compress&cs=tinysrgb&w=600',
+        descripcion: "Este apartamento de lujo está ubicado en una exclusiva zona residencial",
+        ubicacion: 'Magallanes, Chile',
+        habitaciones: 6,
+        banos: 3,
+        costo: 3000,
+        smoke: false,
+        pets: true
+    },
+];
+    export const propiedades_venta =[
+        {
+            nombre: 'Casa en venta 1',
+            src: 'https://images.pexels.com/photos/280222/pexels-photo-280222.jpeg?auto=compress&cs=tinysrgb&w=600',
+            descripcion: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit.',
+            ubicacion: 'Los Lagos, Chile',
+            habitaciones: 3,
+            banos: 2,
+            costo: 1000,
+            smoke: false,
+            pets: true
+        },
+        {
+            nombre: 'Casa en venta 2',
+            src: 'https://images.pexels.com/photos/221540/pexels-photo-221540.jpeg?auto=compress&cs=tinysrgb&w=600',
+            descripcion: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit.',
+            ubicacion: 'Los Rios, Chile',
+            habitaciones: 4,
+            banos: 2,
+            costo: 3000,
+            smoke: true,
+            pets: true
+        },
+        {
+            nombre: 'Casa en venta 3',
+            src: 'https://images.pexels.com/photos/277667/pexels-photo-277667.jpeg?auto=compress&cs=tinysrgb&w=800',
+            descripcion: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit.',
+            ubicacion: 'Los Rios, Chile',
+            habitaciones: 6,
+            banos: 4,
+            costo: 2000,
+            smoke: false,
+            pets: false
+        }
+    ];
